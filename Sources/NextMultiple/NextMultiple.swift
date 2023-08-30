f14c32fa8f099e05e321e4046626b2038ab352f4
Nikita <nicandreo@gmail.com>
2023-08-30 12:26:27 +0400

    add static shared

--- Sources/NextMultiple/NextMultiple.swift
+++ Sources/NextMultiple/NextMultiple.swift
@@ -1,6 +1,8 @@
 
 /// This is a solution for the given task
 public struct NextMultiple {
+    
+    static let shared = NextMultiple()
 
     func nextMultiple(dividend: Int, divisor: Int) -> Int {
         let result = dividend - dividend % abs(divisor)
